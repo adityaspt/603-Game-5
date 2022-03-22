bfe2d8b56876a53a7121155f757690a4ddd66e3c
Aditya Patil <adityaspt@gmail.com>
2022-03-22 17:44:33 -0400

    Player Animation fixed

--- 603 Game 5 Group 2/Assets/PlayerController.cs
+++ 603 Game 5 Group 2/Assets/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     [Range(0f, 15f)]
     private float moveSpeed;
 
+    [SerializeField]
+    Animator playerAnimator;
+
     private Rigidbody2D rdb2;
 
     private Vector2 movement;
@@ -16,6 +19,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         rdb2 = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -24,13 +28,83 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    float x = 0;
+    float y = 0;
+
+
+    [SerializeField]
+    float lastX = 0; //To check if current x value has changed from last
+    [SerializeField]
+    float lastY = 0; //To check if current y value has changed from last
+
+    [SerializeField]
+    bool isFacingUp, isFacingRight, isFacingLeft = false;
+
+    bool isFacingDown = true;
+
     // Update is called once per frame
     void Update()
     {
-        //Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        ////Input
+        //movement.x = Input.GetAxisRaw("Horizontal");
+        //movement.y = Input.GetAxisRaw("Vertical");
+
+        //if (movement.x == 0 && movement.y==0)
+        //{
+        //    playerAnimator.SetBool("isMoving", false);
+        //}
+        //else
+        //{
+        //    playerAnimator.SetBool("isMoving", true);
+        //    playerAnimator.SetFloat("dirY", movement.y);
+
+        //}
+
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        playerAnimator.SetFloat("dirX", movement.x);
+        playerAnimator.SetFloat("dirY", movement.y);
+        playerAnimator.SetFloat("Speed", movement.sqrMagnitude);
+
+        string LastKeyUsed = ""; //will know which axis was changed
+
+        if (x != lastX) //if current x value has changed, we set the last used axis to X and set lastX to current x value
+        {
+            LastKeyUsed = "X";
+            lastX = x;
+
+        }
+        if (y != lastY) //if current y value has changed, we set the last used axis to Y and set lastY to current y value
+        {
+            LastKeyUsed = "Y";
+            lastY = y;
+            
+        }
+
+        if (x != 0 && y == 0) //In case you stop using the last key and have still an other key pressed, you come back to the other key axis
+        {
+            LastKeyUsed = "X";
+        }
+        if (x == 0 && y != 0)
+        {
+            LastKeyUsed = "Y";
+        }
+
+
+        if (LastKeyUsed == "X") //Then we check which axis was last used, and we set movement x and y value depending on the result
+        {
+            movement.x = x;
+            movement.y = 0;
+        }
+        else if (LastKeyUsed == "Y")
+        {
+            movement.x = 0;
+            movement.y = y;
+        }
+
     }
+
 
     private void FixedUpdate()
     {
