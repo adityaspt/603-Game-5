8170fb897796f31074cf56a948e8e03e381c3801
mmgaw113 <89485705+mmgaw113@users.noreply.github.com>
2022-03-30 21:07:58 -0400

    Car Updates

--- 603 Game 5 Group 2/Assets/Scripts/DrivingController.cs
+++ 603 Game 5 Group 2/Assets/Scripts/DrivingController.cs
@@ -6,30 +6,59 @@ public class DrivingController : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
-    private float inputHorizontal;
-    private float inputVertical;
-
+    private Vector2 movement;
+    public bool facingRight = true;
+    public bool facingUp = true;
+    public Quaternion right;
+    public Quaternion left;
+    public Quaternion up;
+    public Quaternion down;
     // Start is called before the first frame update
     void Awake()
     {
+        up = new Quaternion(0f, 0f, 90f, 0f);
+        down = new Quaternion(0f, 0f, -90f, 0f);
+        left = new Quaternion(0f, 0f, 180f, 0f);
+        right = new Quaternion(0f, 0f, 0f, 0f);
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputHorizontal = Input.GetAxisRaw("Horizontal");
-        inputVertical = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+/*        if (movement.x > 0)
+        {
+            transform.rotation = right;
+        }
+        if(movement.x < 0)
+        {
+            transform.rotation = left;
+        }
+        if(movement.y > 0)
+        {
+            transform.rotation = down;
+        }
+        if(movement.y > 0)
+        {
+            transform.rotation = up;
+        }*/
     }
+
     private void FixedUpdate()
     {
-        if(inputHorizontal != 0 || inputVertical != 0)
-        {
-            rb.velocity = new Vector2 (inputHorizontal * speed, inputVertical * speed);
-        }
-        else
-        {
-            rb.velocity = new Vector2(0f,0f);
-        }
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+    public void Flip()
+    {
+        transform.Rotate(0f, 0f, 180f);
+        facingRight = !facingRight;
+    }
+    public void FacingUp()
+    {
+        transform.Rotate(0f, 0f, 90f);
+        facingUp = !facingUp;
     }
 }
