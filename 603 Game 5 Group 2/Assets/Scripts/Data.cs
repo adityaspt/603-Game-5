4e778e8d452d669e3b748e1ce0ad08d40cef3aaf
Mason Gaw <89485705+mmgaw113@users.noreply.github.com>
2022-04-02 10:30:38 -0400

    Data Collection and Driving Updates

--- 603 Game 5 Group 2/Assets/Scripts/Data.cs
+++ 603 Game 5 Group 2/Assets/Scripts/Data.cs
@@ -10,10 +10,10 @@ public class Data
 
     public Data(Player player)
     {
-        player.name1 = namesOfPeople[0];
-        player.name2 = namesOfPeople[1];
+        //player.name1 = namesOfPeople[0];
+        //player.name2 = namesOfPeople[1];
         player.deathCount = deathCount;
-        player.name1 = dead[0];
+        //player.name1 = dead[0];
         //deathCount;
 /*        foreach(string name in namesOfPeople)
         {
--- 603 Game 5 Group 2/Assets/Scripts/DrivingController.cs
+++ 603 Game 5 Group 2/Assets/Scripts/DrivingController.cs
@@ -9,18 +9,10 @@ public class DrivingController : MonoBehaviour
     private Vector2 movement;
     public bool facingRight = true;
     public bool facingUp = true;
-    public Quaternion right;
-    public Quaternion left;
-    public Quaternion up;
-    public Quaternion down;
     public Animator anim;
     // Start is called before the first frame update
     void Awake()
     {
-        up = new Quaternion(0f, 0f, 90f, 0f);
-        down = new Quaternion(0f, 0f, -90f, 0f);
-        left = new Quaternion(0f, 0f, 180f, 0f);
-        right = new Quaternion(0f, 0f, 0f, 0f);
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
@@ -31,38 +23,27 @@ public class DrivingController : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         anim.SetFloat("Movement_X", movement.x);
-        anim.SetFloat("Movement_Y", movement.y);
+        //anim.SetFloat("Movement_Y", movement.y);
         anim.SetFloat("Speed", movement.sqrMagnitude);
-/*        if (movement.x > 0)
+        Debug.Log(movement.sqrMagnitude);
+      if (movement.x > 0 && !facingRight)
         {
-
+            Flip();
         }
-        if(movement.x < 0)
+        if(movement.x < 0 && facingRight)
         {
-
+            Flip();
         }
-        if(movement.y > 0)
-        {
-
-        }
-        if(movement.y > 0)
-        {
-
-        }*/
     }
-
+    void Flip()
+    {
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
+    }
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
-    }
-    public void Flip()
-    {
-        transform.Rotate(0f, 0f, 180f);
-        facingRight = !facingRight;
-    }
-    public void FacingUp()
-    {
-        transform.Rotate(0f, 0f, 90f);
-        facingUp = !facingUp;
     }
 }
